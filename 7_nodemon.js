a2866d1089f02838a176df2f2ce5da6f57166fbf
Zubair Shahid <gokublack008@protonmail.com>
2023-04-18 14:11:10 +0500

    new file

---
+++ 7_nodemon.js
@@ -0,0 +1,11 @@
+// jb mn complex task perform kr rhy hoty hain ya jb hum API py kaam kr rhy hoty hain to hmein baar baar changes krny k baad project ko run krna parta hai.
+// bar bar run krny ki wajha se bhht time waste hota hai.
+// iss se bachny k liye hum "nodemon" package ka use krty hain.
+// Installation: "npm i nodemon".
+// or nodemon ko istall krny k baad file ko "nodemon ./index.js" iss tarhan chalana hai.
+// "node ./index.js" ab purana tareeka ho gya hai.
+// command for global installation: "npm i nodemon -g".
+// ab mn nodemon ko install krny laga hn.
+// Node Js async hoti hai.
+
+console.log("Hello Nodemon.");
